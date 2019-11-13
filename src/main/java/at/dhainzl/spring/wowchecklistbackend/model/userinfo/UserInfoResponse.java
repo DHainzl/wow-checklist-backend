9fb27151d8a3e34d81af61285377810b3d7506df
David Hainzl <david@dhainzl.at>
2019-11-13 10:39:51 +0100

    feat(user-info): Adds userinfo endpoint

---
+++ src/main/java/at/dhainzl/spring/wowchecklistbackend/model/userinfo/UserInfoResponse.java
@@ -0,0 +1,21 @@
+package at.dhainzl.spring.wowchecklistbackend.model.userinfo;
+
+import java.util.Map;
+
+public class UserInfoResponse {
+    public String battletag = "";
+
+    public static UserInfoResponse fromAuthDetails(Object authDetails) {
+        UserInfoResponse userInfo = new UserInfoResponse();
+
+        if (!(authDetails instanceof Map)) {
+            return userInfo;
+        }
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> authDetailsMap = (Map<String, Object>) authDetails;
+        userInfo.battletag = (String) authDetailsMap.getOrDefault("battletag", "");
+
+        return userInfo;
+    }
+}
---
+++ src/main/java/at/dhainzl/spring/wowchecklistbackend/rest/UserInfoController.java
@@ -0,0 +1,23 @@
+package at.dhainzl.spring.wowchecklistbackend.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.dhainzl.spring.wowchecklistbackend.configuration.authentication.IAuthenticationFacade;
+import at.dhainzl.spring.wowchecklistbackend.model.userinfo.UserInfoResponse;
+
+@RestController
+public class UserInfoController {
+    @Autowired
+    private IAuthenticationFacade authentication;
+    
+    @GetMapping(path = "/userinfo", produces = MediaType.APPLICATION_JSON_VALUE)
+    public UserInfoResponse getUserInfo() {
+        OAuth2Authentication auth = (OAuth2Authentication) authentication.getAuthentication();
+
+        return UserInfoResponse.fromAuthDetails(auth.getUserAuthentication().getDetails());
+    }
+}
