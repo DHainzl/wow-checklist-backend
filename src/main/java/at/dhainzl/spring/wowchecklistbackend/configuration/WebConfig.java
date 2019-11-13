8cfdf6276123bd7712131f56d10fa73bddf03226
David Hainzl <david@dhainzl.at>
2019-11-13 20:11:08 +0100

    feat: Enables CORS headers

---
+++ src/main/java/at/dhainzl/spring/wowchecklistbackend/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package at.dhainzl.spring.wowchecklistbackend.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${app.allowedCorsOrigins}")
+    String allowedCorsOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins(allowedCorsOrigins);
+    }
+}
