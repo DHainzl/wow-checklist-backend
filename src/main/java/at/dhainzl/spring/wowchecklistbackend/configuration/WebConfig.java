4f37d537ad5b3fd6418de1e8a14266606074f345
David Hainzl <david@dhainzl.at>
2020-12-04 22:35:05 +0100

    chore: Try to add same site cookie policy

--- src/main/java/at/dhainzl/spring/wowchecklistbackend/configuration/WebConfig.java
+++ src/main/java/at/dhainzl/spring/wowchecklistbackend/configuration/WebConfig.java
@@ -1,6 +1,10 @@
 package at.dhainzl.spring.wowchecklistbackend.configuration;
 
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${app.allowedCorsOrigins}")
     String[] allowedCorsOrigins;
 
+    @Bean
+    public TomcatContextCustomizer sameSiteCookiesConfig() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
+    }
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
